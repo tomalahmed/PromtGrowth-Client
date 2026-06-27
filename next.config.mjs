44958599ba762a3504fa42d5d210407e9ced7768
TOMAL <tomal7673@gmail.com>
2026-06-27 15:45:56 +0600

    fix login issue oAuth

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const securityHeaders = [
   { key: "X-Content-Type-Options", value: "nosniff" },
   { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
   {
+    key: "Cross-Origin-Opener-Policy",
+    value: "same-origin-allow-popups",
+  },
+  {
     key: "Permissions-Policy",
     value: "camera=(), microphone=(), geolocation=()",
   },
